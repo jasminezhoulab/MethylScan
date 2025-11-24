5a71918d281db28b5c5a6c2bc106834a93915ddc
jasminezhoulab <26192143+jasminezhoulab@users.noreply.github.com>
2025-11-23 21:46:37 -0800

    Delete demo/step0_ensure_R_and_libraries.sh

--- demo/step0_ensure_R_and_libraries.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "NumPy version:     $(python -c 'import numpy as np; print(np.__version__)' 2>/dev/null || echo "Not installed")"
-echo "Pandas version:    $(python -c 'import pandas as pd; print(pd.__version__)' 2>/dev/null || echo "Not installed")"
-echo "scikit-learn version: $(python -c 'import sklearn; print(sklearn.__version__)' 2>/dev/null || echo "Not installed")"
-
